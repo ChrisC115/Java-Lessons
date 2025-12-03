59e7a5c7ec09fa2853bf9c0b360c89a97f19c2ce
ChrisC115 <notebooklm2000@gmail.com>
2025-12-03 13:28:28 +0000

    Dec 3

---
+++ fundamentals/DataTypes.java
@@ -0,0 +1,23 @@
+// java is a Strongly Types Language: you have to Define/Declare a variable
+// you have to declare its data type
+
+
+class DataTypeExamples {
+
+    int myInt = 42;
+    double myDouble = 189.75;
+    char myChar = 'Q';
+    boolean myBool = false;
+
+}
+
+
+public class DataTypes{
+    public static void main(String[] args){
+
+        DataTypeExamples datatype = new DataTypeExamples();
+
+        System.out.println("myInt:" + datatype.myInt);
+
+    }
+}
