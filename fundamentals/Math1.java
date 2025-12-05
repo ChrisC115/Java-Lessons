7dafe5819f543231bca519d66999c1b3701eb6c1
ChrisC115 <notebooklm2000@gmail.com>
2025-12-05 15:48:07 +0000

    Maths

--- fundamentals/Math1.java
+++ fundamentals/Math1.java
@@ -5,12 +5,20 @@ class Calculator {
         return a + b;
     }
 
+    double add(double a, double b) {
+        return a + b;
+    }
+
     int subtract(int a, int b) {
         return a -b;
     }
 
     int multiply(int a, int b) {
         return a * b;
+    }
+
+    int multiply(int a, int b, int c) {
+        return a * b * c;
     }
 
     int divide(int a, int b) {
@@ -36,10 +44,12 @@ public class Math1 {
         Calculator calc = new Calculator();
 
         System.out.println(calc.add(10, 5));
+        System.out.println(calc.add(3.2, 4.8));
         System.out.println(calc.subtract(10, 5));
         System.out.println(calc.multiply(10, 5));
+        System.out.println(calc.multiply(2, 3, 4));
         System.out.println(calc.divide(10, 5));
-        System.out.println(calc.divide(1, 8));
+        System.out.println(calc.divide(1.0, 8.0));
         System.out.println(calc.modulo(10, 4));
 
     }
