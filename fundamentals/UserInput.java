daa28ae39420c4e60f10796bb8b569237f2f0ccc
ChrisC115 <notebooklm2000@gmail.com>
2025-12-11 15:38:38 +0000

    CHallenges 1 and 2 are done

---
+++ fundamentals/UserInput.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your name");
+        String name = input.nextLine();
+        System.out.println(name);
+
+        System.out.println("Enter your age");
+        String age = input.nextLine();
+
+        int ageConverted = Integer.parseInt(age);
+        System.out.println("Next year " + name + " will be: " + (ageConverted + 1));
+
+        System.out.println("What is your height in meters?");
+        double height = Double.parseDouble(input.nextLine());
+        System.out.println("You are " + height + " meters tall.");
+
+    }
+}
+//Challenge 1
+
+class Email {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("What are the words in your email adress");
+        String[] email = new String[] { input.nextLine(), input.nextLine()}; 
+        int[] number = {Integer.parseInt(input.nextLine()), Integer.parseInt(input.nextLine())};
+        int numberBest = number[0] + number[1];
+
+        System.out.println("Your email is " + email[0] + email[1] + numberBest + "@gmail.com");
+
+    }
+
+//Challenge 2
+
+class Numbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What are your two numbers");
+        double[] numbers = new double[] {Double.parseDouble(input.nextLine()), Double.parseDouble(input.nextLine())};
+        Double newNumber = numbers[0] / numbers[1];
+
+        System.out.println("You are " + newNumber + " meters tall.");
+
+    }
+
+}
+
+}
