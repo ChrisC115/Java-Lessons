dbd90f801795eb4376579ceec70b09710fbb7fde
ChrisC115 <notebooklm2000@gmail.com>
2026-01-12 13:36:40 +0000

    Arrays  Finish Later

---
+++ Arrays/PracticeArray.java
@@ -0,0 +1,25 @@
+public class PracticeArray {
+    //Challenge 1
+    int[] price = new int[10];
+
+    //Challenge 2
+    int[] number = new int[20];
+    for(int i=0; i < number.length; i++) {
+        price[i] = 4;
+
+    //Challenge 3
+    int[] numbers = new int[20];
+        if((numbers.length % 2) == 0) {
+            int median1 = numbers.length / 2;
+            System.out.println("The Medians are " + numbers[median1] + "and " + numbers[median1 + 1]);
+        } else {
+           int median2 = numbers.length 
+        }
+        
+
+
+
+
+    }
+}}
+
